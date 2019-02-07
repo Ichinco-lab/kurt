06cfebedf9cc11d581502ffea740bdaeebe23fd9
Ichinco <michelle_ichinco@c02x34rljg5j.uml.edu>
2019-02-07 14:45:25 -0500

    fixed file structure so we don't need local paths, added file for complexity tests

--- kurt/KurtToCSV.py
+++ kurt/KurtToCSV.py
@@ -1,7 +1,7 @@
-""""importing kurt and local files. Ideally I want the more generic path 
+""""importing kurt and local files. Ideally I want the more generic path
 	but python hasn't been cooperating so I need fix this later"""
 import sys
-sys.path.append("/home/aaron/Programs/python/kurt/")
+#sys.path.append("/Users/michelle_ichinco/Documents/Research/kurt/tests")
 import kurt
 
 class KurtToCSV:
@@ -10,7 +10,7 @@ class KurtToCSV:
 	"""Default Constructor"""
 	def __init__(self):
 		return
-	
+
 
 	"""Returns the name or 'id' of the project"""
 	def KurtToCSVgetFileName(self,project):
@@ -24,7 +24,7 @@ class KurtToCSV:
 
 	"""returns if a sprite is the stage or a regular sprite"""
 	def KurtToCSVSpriteOrBackground(self,sprite):
-		if self.KurtToCSVgetSpriteName(sprite) == "Stage": 
+		if self.KurtToCSVgetSpriteName(sprite) == "Stage":
 			return "STAGE"
 		return "SPRITE"
 
@@ -32,7 +32,7 @@ class KurtToCSV:
 	"""returns the string form of the event in a script"""
 	def KurtToCSVgetEvent(self,script):
 		#the 1st block always contains the event
-		event = script[0].__repr__()	
+		event = script[0].__repr__()
 		#here we strip excess characters from the event
 		event = event[2:] #since the event/hat block is never indented this
 		#line will always work as intended
@@ -45,9 +45,9 @@ class KurtToCSV:
 			if character == "'":
 				break
 			endStrip += 1
-		event = event[0:endStrip]	
+		event = event[0:endStrip]
 		return event
-		
+
 
 
 	"""returns the number of movement blocks in a script"""
@@ -55,20 +55,20 @@ class KurtToCSV:
 		movement = 0
 		for block in script:
 			movement += self.KurtToCSVisMovementBlock(block)
-		return movement		
-		
+		return movement
+
 
 	"""helper function returns true if a block is a movement block
 		returns false otherise"""
 	def KurtToCSVisMovementBlock(self,block):
-		movements = {'forward:', 'turnRight:', 'turnLeft:', 'heading:', 'pointTowards:', 'gotoX:y:', 'gotoSpriteOrMouse:', 'glideSecs:toX:y:elapsed:from:', 'changeXposBy:', 'xpos:', 'changeYposBy:', 'ypos:', 'bounceOffEdge', 'setRotationStyle'}	
-		count = 0	
-		strBlock = block.__repr__()	
+		movements = {'forward:', 'turnRight:', 'turnLeft:', 'heading:', 'pointTowards:', 'gotoX:y:', 'gotoSpriteOrMouse:', 'glideSecs:toX:y:elapsed:from:', 'changeXposBy:', 'xpos:', 'changeYposBy:', 'ypos:', 'bounceOffEdge', 'setRotationStyle'}
+		count = 0
+		strBlock = block.__repr__()
 		for movement in movements:
 			count += self.KurtToCSVfindNumInstancesInStr(strBlock,movement)
 		return count
-		
-	"""returns the number of times a given movement is contained 
+
+	"""returns the number of times a given movement is contained
 	within a block"""
 	def KurtToCSVfindNumInstancesInStr(self,strBlock,movement):
 		words = strBlock.split()
@@ -76,7 +76,7 @@ class KurtToCSV:
 		for word in words:
 			if movement in word:
 				count += 1
-		return count  
+		return count
 
 
 	"""returns the number of blocks in a script"""
@@ -95,10 +95,10 @@ class KurtToCSV:
 			if "doUntil" in line:
 				numLines -= 2
 		return numLines - 1
-		
-		
+
+
 	"""returns the maximum level of nesting found in a script"""
-	#TO DO	
+	#TO DO
 	def KurtToCSVgetScriptNestLevel(self,script):
 		#3 potential methods:
 		#either use a recursie function to keep track of nesting
@@ -116,7 +116,7 @@ class KurtToCSV:
 		f.write("project,object,backgroundOrSprite,script_id,event," + "levelsOfNesting,numberOfMovementBlocks,lengthOfScript,script\n")
 		for sprite in project.sprites + [project.stage]:
 			scriptCounter = 0
-			for script in sprite.scripts:	
+			for script in sprite.scripts:
 				line = self.KurtToCSVgetFileName(project) + ","
 				line += self.KurtToCSVgetSpriteName(sprite) + ","
 				line += self.KurtToCSVSpriteOrBackground(sprite) + ","
@@ -124,7 +124,7 @@ class KurtToCSV:
 				line += self.KurtToCSVgetEvent(script) + ","
 				line += str(self.KurtToCSVgetScriptNestLevel(script)) + ","
 				line += str(self.KurtToCSVgetNumMovementBlocks(script)) + ","
-				line += str(self.KurtToCSVgetScriptLength(script)) + ',"'			
+				line += str(self.KurtToCSVgetScriptLength(script)) + ',"'
 				line += script.__repr__() + '"\n'
 				f.write(line)
 				scriptCounter += 1
@@ -142,18 +142,16 @@ else:
 	block3
 	block4
 	...
-	
+
 In the example above, the if else statement is stored within the script as a
 single block
 
-2. continue refactor the getScriptLength function due to finding the issues 
-	documented directly above after initially coding said function 
-	--improved by counting the new lines. Should work much better 
+2. continue refactor the getScriptLength function due to finding the issues
+	documented directly above after initially coding said function
+	--improved by counting the new lines. Should work much better
 	than the previous version although still needs tweaking due to being slightly
 	off in control statements. (some use different amounts of newlines when
 	they have blocks inside them than if not) To be clear, this could be fixed
-	if I tested to see if a line contains a block keyword but this seems 
+	if I tested to see if a line contains a block keyword but this seems
 	inefficient and there is most certainly a better way.--
 """
-
-
