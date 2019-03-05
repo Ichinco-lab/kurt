7e3ec84d2a1162d778033877511593038c0c20ef
Shruti Jain <35978103+shrutijain18@users.noreply.github.com>
2019-03-05 16:37:11 -0500

    Multiple Plugins
    The multiple plug in feeds to the 'args'.  'Args'is then passed intoHairball as an argument.

--- hairball/hairball-master/hairball/all_in_one.py
+++ hairball/hairball-master/hairball/all_in_one.py
@@ -1,16 +1,19 @@
 import kurt
 import os
 import sys
+import pdb
 from plugins import HairballPlugin
 from plugins import blocks
 from hairball import Hairball
 
-hairball1 = Hairball(sys.argv[1:])
+# pdb.set_trace()
+args = ['-p', 'masteryNEU.Mastery', 'blocks.BlockCounts', 'test/tmp.sb']
+hairball1 = Hairball(args)
 hairball1.initialize_plugins()
 hairball1.process()
 hairball1.finalize()
-scratchFile = "test/tmp.sb"
-outFile = "testresult.csv"
-PC = blocks.BlockCounts()
-print PC.finalize()
+# scratchFile = "test/tmp.sb"
+# outFile = "testresult.csv"
+# hairball1.BlockCounts()
+# print PC.finalize()
 # print PC.analyze(scratchFile)
