a74ebd429535ea9e3e382fac8b11cb3674c6778f
Shruti Jain <35978103+shrutijain18@users.noreply.github.com>
2019-03-05 17:01:33 -0500

    Multiple Files and Multiple Plugins
    Turns out every plugins uses process() and finalize() in their class. This is the code to iiterate multiple plugins over multiple files.

--- hairball/hairball-master/hairball/all_in_one.py
+++ hairball/hairball-master/hairball/all_in_one.py
@@ -6,10 +6,11 @@ from plugins import HairballPlugin
 from plugins import blocks
 from hairball import Hairball
 
-
-files = ['test/tmp.sb', 'file2', 'file3']
+#To take in multiple files
+files = ['test/tmp.sb', 'test/testsj1.sb2', 'test/testsj2_empty_if.sb2']
+#To iterate over all the files and analyze all the plugins on each one of them
 for file in files:
-    args = ['-p', 'masteryNEU.Mastery', '-p', 'blocks.BlockCounts', file]
+    args = ['-p', 'masteryNEU.Mastery', '-p', 'blocks.BlockCounts', '-p', 'blocks.DeadCode', file]
     hairball1 = Hairball(args)
     hairball1.initialize_plugins()
     hairball1.process()
